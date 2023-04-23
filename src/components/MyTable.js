3a2335955d84d45559609e04655451446a19ea2e
LucasMarelli <lucasmarelli90@gmail.com>
2023-04-23 20:18:33 -0300

    update

--- src/components/MyTable.js
+++ src/components/MyTable.js
@@ -1,27 +1,29 @@
 import { Paper, Table, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
-import moment from "moment";
 import { DataGrid } from '@mui/x-data-grid';
-import { groupAndSort } from "../utils/groupAndSort";
 
-export default function MyTable({ data ,devices}) {
+export default function MyTable({ data, devices }) {
     if (!data?.length) return (<div>No data</div>)
     console.log("data", data)
     const columns = devices.map((device) => {
         return {
             field: device.name,
-            width: 150,
+            headerName: `${device.name} [${device.unit}]`,
+            width: 250,
             headerAlign: 'center',
             align: 'center'
         }
     })
-    columns.unshift({ field: "dateTime", width: 150, headerName: "Tiempo" })
-    console.log("columns", columns)
+    columns.unshift({
+        field: "dateTime", width: 150,
+        headerName: "Tiempo",
+        headerAlign: 'center',
+        align: 'center'
+    })
     return (
         <div style={{ height: 400, width: '100%' }}>
             <DataGrid
                 density="compact"
                 showCellVerticalBorder={true}
-
                 rows={data}
                 columns={columns}
                 pageSize={5}
